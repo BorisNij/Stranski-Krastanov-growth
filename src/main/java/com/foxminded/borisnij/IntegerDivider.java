92f5fd7d2ddeb5096c3a416f887dfba7fff85a4f
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-08-17 08:14:07 -0400

    fix: fix divider not recording digit 0 in quotient

--- src/main/java/com/foxminded/borisnij/IntegerDivider.java
+++ src/main/java/com/foxminded/borisnij/IntegerDivider.java
@@ -23,6 +23,11 @@ public class IntegerDivider {
                                                   Character.forDigit(quotientDigit, 10)));
 
                 partialDividend -= divisorMultiple;
+            } else if (!steps.isEmpty()) {
+                // Add the step with quotient digit 0 even if partialDividend < divisor
+                steps.add(new IntegerDivisionStep(partialDividend,
+                                                  0,
+                                                  Character.forDigit(0, 10)));
             }
         }
         steps.add(new IntegerDivisionStep(partialDividend, 0, '\0'));
