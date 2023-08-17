b3ab963be56176daa3dcfeb65d49317a2f7d2116
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-08-17 08:50:10 -0400

    refactor: extract the update steps method

--- src/main/java/com/foxminded/borisnij/IntegerDivider.java
+++ src/main/java/com/foxminded/borisnij/IntegerDivider.java
@@ -7,7 +7,6 @@ public class IntegerDivider {
 
     public List<IntegerDivisionStep> divide(int dividend, int divisor) {
         List<IntegerDivisionStep> steps = new ArrayList<>();
-
         String dividendStr = String.valueOf(dividend);
         int partialDividend = 0;
 
@@ -18,19 +17,23 @@ public class IntegerDivider {
             if (partialDividend >= divisor) {
                 int quotientDigit = partialDividend / divisor;
                 int divisorMultiple = quotientDigit * divisor;
-                steps.add(new IntegerDivisionStep(partialDividend,
-                                                  divisorMultiple,
-                                                  Character.forDigit(quotientDigit, 10)));
+                updateSteps(steps, partialDividend, divisorMultiple, Character.forDigit(quotientDigit, 10));
 
                 partialDividend -= divisorMultiple;
             } else if (!steps.isEmpty()) {
-                // Add the step with quotient digit 0 even if partialDividend < divisor
-                steps.add(new IntegerDivisionStep(partialDividend,
-                                                  0,
-                                                  Character.forDigit(0, 10)));
+                updateSteps(steps, partialDividend, 0, Character.forDigit(0, 10));
             }
         }
-        steps.add(new IntegerDivisionStep(partialDividend, 0, '\0'));
+
+        updateSteps(steps, partialDividend, 0, '\0');
         return steps;
     }
+
+    private void updateSteps(List<IntegerDivisionStep> steps,
+                             int partialDividend,
+                             int divisorMultiple,
+                             char quotientDigit) {
+        steps.add(new IntegerDivisionStep(partialDividend, divisorMultiple, quotientDigit));
+    }
+
 }
