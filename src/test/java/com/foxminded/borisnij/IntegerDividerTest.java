9d4f6dda42079a7c818e8fb4e45060ba9e934b13
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-08-17 20:03:24 -0400

    test: add test and edge cases

--- src/test/java/com/foxminded/borisnij/IntegerDividerTest.java
+++ src/test/java/com/foxminded/borisnij/IntegerDividerTest.java
@@ -1,6 +1,8 @@
 package com.foxminded.borisnij;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,10 +12,13 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IntegerDividerTest {
+    IntegerDivider divider;
 
     static Stream<Arguments> testData() {
+
         return Stream.of(
                 Arguments.of(167, 7, Arrays.asList(
                         new IntegerDivisionStep(16, 14, '2'),
@@ -29,17 +34,48 @@ class IntegerDividerTest {
                         new IntegerDivisionStep(3, 0, '0'),
                         new IntegerDivisionStep(38, 35, '5'),
                         new IntegerDivisionStep(3, 0, '\0')
+                )),
+                Arguments.of(1, 1, Arrays.asList(
+                        new IntegerDivisionStep(1, 1, '1'),
+                        new IntegerDivisionStep(0, 0, '\0')
+                )),
+                Arguments.of(0, 1, Arrays.asList(
+                        new IntegerDivisionStep(0, 0, '0'),
+                        new IntegerDivisionStep(1, 0, '\0')
                 ))
         );
+    }
+
+    @BeforeEach
+    void init() {
+        divider = new IntegerDivider();
     }
 
     @ParameterizedTest
     @MethodSource("testData")
     @DisplayName("When provided with valid dividend and divisor should return correct division steps")
     void whenProvidedWithValidDividendAndDivisorShouldReturnCorrectDivisionSteps(int dividend, int divisor, List<IntegerDivisionStep> expectedSteps) {
-        IntegerDivider divider = new IntegerDivider();
         List<IntegerDivisionStep> actualSteps = divider.divide(dividend, divisor);
 
         assertEquals(expectedSteps, actualSteps);
+    }
+
+    @Test
+    @DisplayName("When provided with divisor 0 should throw IllegalArgumentException")
+    void whenProvidedWithDivisor0ShouldThrowIllegalArgumentException() {
+
+        assertThrows(IllegalArgumentException.class, () -> divider.divide(123, 0));
+    }
+
+    @Test
+    @DisplayName("When provided with negative divisor should throw IllegalArgumentException")
+    void whenProvidedWithNegativeDivisorShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> divider.divide(123, -2));
+    }
+
+    @Test
+    @DisplayName("When provided with negative dividend should throw IllegalArgumentException")
+    void whenProvidedWithNegativeDividendShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> divider.divide(-3, 1));
     }
 }
